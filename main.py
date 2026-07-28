66f418b47a25718a7a824624f1aad8b4c7bc6851
tr_jgw <745388985@qq.com>
2026-07-28 09:46:26 +0800

    v0.0.1 测试处理

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+@File: main.py
+@Author: jigangwang
+@Email: wjigang@grupotr.es
+@Date: 2026-07-28
+@Desc: 
+"""
+
 from logger_utils.logger_manager import logger
 
-if __name__ == "__main__":
-    print("main.py")
-    logger.info("hello world")
+def add(x:int, y:int) -> int:
+    """_summary_
+
+    Args:
+        x (int): _description_
+        y (int): _description_
+
+    Returns:
+        int: _description_
+    """
+    try:
+        pass
+        1 / 0
+    except Exception as e:
+        logger.error(f"Error: {e}")
+        
+if __name__ == '__main__':
+    res = add(1, 2)
+    logger.info("test logger")
